e1a80d72943e636dac4fd1bdfa69a79ef8be638e
Maarten Staa <maartens@spotify.com>
2025-08-08 22:40:53 +0200

    Remove unused code

--- src/table/column.rs
+++ src/table/column.rs
@@ -11,13 +11,6 @@ impl ColumnAttributes {
     const RESERVED: u64 = 1 << 2;
     const STRONG_LINKS: u64 = 1 << 3;
     const NULLABLE: u64 = 1 << 4;
-    const LIST: u64 = 1 << 5;
-    const DICTIONARY: u64 = 1 << 6;
-    const SET: u64 = 1 << 7;
-    const FULLTEXT_INDEXED: u64 = 1 << 8;
-
-    /// Mask for the collection type (list, dictionary, set)
-    const COLLECTION_TYPE_MASK: u64 = Self::LIST | Self::DICTIONARY | Self::SET;
 
     pub fn new(attributes: u64) -> Self {
         Self(attributes)
@@ -42,26 +35,6 @@ impl ColumnAttributes {
     pub fn is_nullable(&self) -> bool {
         self.0 & Self::NULLABLE != 0
     }
-
-    pub fn is_list(&self) -> bool {
-        self.0 & Self::LIST != 0
-    }
-
-    pub fn is_dictionary(&self) -> bool {
-        self.0 & Self::DICTIONARY != 0
-    }
-
-    pub fn is_set(&self) -> bool {
-        self.0 & Self::SET != 0
-    }
-
-    pub fn is_fulltext_indexed(&self) -> bool {
-        self.0 & Self::FULLTEXT_INDEXED != 0
-    }
-
-    pub fn is_collection(&self) -> bool {
-        self.0 & Self::COLLECTION_TYPE_MASK != 0
-    }
 }
 
 impl Debug for ColumnAttributes {
@@ -81,18 +54,6 @@ impl Debug for ColumnAttributes {
         }
         if self.is_nullable() {
             s.field("nullable", &true);
-        }
-        if self.is_list() {
-            s.field("list", &true);
-        }
-        if self.is_dictionary() {
-            s.field("dictionary", &true);
-        }
-        if self.is_set() {
-            s.field("set", &true);
-        }
-        if self.is_fulltext_indexed() {
-            s.field("fulltext_indexed", &true);
         }
         s.finish()
     }
